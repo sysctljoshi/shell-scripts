dd9c25461266838edc5cec67ef5c4129fafcecd9
Joshi MP <38281859+sysctljoshi@users.noreply.github.com>
2018-04-11 16:34:39 +0530

    Update previewdbback.sh

--- db_backup/previewdbback.sh
+++ db_backup/previewdbback.sh
@@ -2,9 +2,9 @@
 #!/bin/bash
 today="$(date +%F-%H-%M)"
 
-su -c "/usr/local/pgsql/bin/pg_dump cpv  > /srv/dbback/cpv-$today.out" -s /bin/sh postgres
+su -c "/usr/local/pgsql/bin/pg_dump <DB_Name>  > /srv/dbback/<DB_Name>-$today.out" -s /bin/sh postgres
 
-scp /srv/dbback/cpv-$today.out root@192.168.72.250:/data/NMDC_BACKUP/DATABASES/Preview/
+scp /srv/dbback/cpv-$today.out root@192.168.72.250:/data/NMDC_BACKUP/DATABASES/<Db_Name>
 
 rm -rf /srv/dbback/cpv-$today.out
 
